3340f027a23bd0c0e5683bbc772acb2859c59351
Yusuke Ohi <y.ohi@diamondhead.tech>
2025-09-11 19:12:47 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       },
     },
     // コード分割とバンドル最適化
-    optimizeCss: true,
+    optimizeCss: false, // crittersモジュール要求を回避
     optimizeServerReact: true,
     turbotrace: {
       logLevel: 'error',
